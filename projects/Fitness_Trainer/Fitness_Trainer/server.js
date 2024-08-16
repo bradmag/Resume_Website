ab6e7bd6d2b6a8c063f6dc36f20cfc9c60b3a7f2
bradmag <104655927+bradmag@users.noreply.github.com>
2024-08-16 10:42:29 -0600

    First Draft server.js
    Crates the first draft of a server.
    API to create users

--- projects/Fitness_Trainer/Fitness_Trainer/server.js
+++ projects/Fitness_Trainer/Fitness_Trainer/server.js
@@ -1,1 +1,35 @@
 /* This will hold the fitness server to interact with the database */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let db = new sqlite3.Database('../../../../../SQL/Databases/Workout_Database/workout_database.db', (err) => {
+    if(err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the Workout Database.');
+});
+
+app.post('/register', (req, res) => {/* register a new user*/
+    const { username, email, password } = req.body;
+
+    const sql = 'INSERT INTO Users (username, email, password, created_at, updated_at) VALUES (?, ?, ?, datetime("now"), datetime("now"))';
+    db.run(sql, [username, email, password], function(err) {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": { id: this.lastID, username, email }
+        });
+    });
+});
